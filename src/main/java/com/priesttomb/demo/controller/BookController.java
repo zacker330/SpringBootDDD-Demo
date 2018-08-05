0afc4b7a64b5eedc393fbbb13ea8876f6cbccd1b
PriestTomb <mxingzh@163.com>
2018-08-05 14:05:42 +0800

    注释、格式化

--- src/main/java/com/priesttomb/demo/controller/BookController.java
+++ src/main/java/com/priesttomb/demo/controller/BookController.java
@@ -19,23 +19,41 @@ public class BookController {
 	@Autowired
 	private IBookService bookService;
 	
+	/**
+	 * 添加一本书
+	 * @param name 书名
+	 * @param price 书价格
+	 * @return 新书
+	 */
 	@PostMapping(value = "/book")
 	public Book addBook(@RequestParam(value="name") String name,@RequestParam(value="price") Integer price) {
 		Book book = new Book(name, price);
-		Book book2 = bookService.saveBook(book);
-		return book2;
+		return bookService.saveBook(book);
 	}
 	
+	/**
+	 * 获取所有的书
+	 * @return
+	 */
 	@GetMapping(value = "/book")
 	public List<Book> getAllBooks() {
 		return bookService.getAllBooks();
 	}
 	
+	/**
+	 * 根据书的id获取书
+	 * @param id
+	 * @return
+	 */
 	@GetMapping(value = "/book/{id}")
 	public Book getBookById(@PathVariable("id") Integer id) {
 		return bookService.getBookById(id);
 	}
 	
+	/**
+	 * 根据书的id删除书
+	 * @param id
+	 */
 	@DeleteMapping(value = "/book/{id}")
 	public void delBookById(@PathVariable("id") Integer id) {
 		bookService.delBookById(id);
